fb2df123c5b93f1eb3bc98c287096d8d816d56eb
YeonSooYeon <dustndus8@naver.com>
2021-08-06 01:45:03 +0900

    refactor: 바텀 네비게이션 수정

--- AndroidStudio/app/src/main/java/com/jarvas/mappyapp/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/jarvas/mappyapp/MainActivity.java
@@ -1,35 +1,52 @@
 package com.jarvas.mappyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.SearchView;
+import android.view.MenuItem;
 
-import net.daum.mf.map.api.MapView;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView; //바텀 네비게이션 뷰
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNAV);
 
-        MapView mapView = new MapView(this);
+        //첫화면
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame,new Fragment_map()).commit();
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+        // 바텀 네비게이션 뷰 안 아이템 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        SearchView searchView = findViewById(R.id.search_view);
-        searchView.bringToFront();
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+                switch(menuItem.getItemId()){
+                    case R.id.map:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment_map()).commit();
+                        break;
+                    case R.id.stars:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment_stars()).commit();
+                        break;
+                    case R.id.setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment_setting()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+
+
+        //SearchView searchView = findViewById(R.id.search_view);
+        //searchView.bringToFront();
+
     }
 
 }
