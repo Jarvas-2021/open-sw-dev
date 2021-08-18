260e04c239fb0a050c1f22f51874b6093c7bd4b9
YeonSooYeon <dustndus8@naver.com>
2021-08-18 16:54:30 +0900

    feat: 검색창 레이아웃 및 클래스 추가

---
+++ AndroidStudio/app/src/main/java/com/jarvas/mappyapp/api/ApiInterface.java
@@ -0,0 +1,39 @@
+package com.jarvas.mappyapp.api;
+
+import com.jarvas.mappyapp.model.category_search.CategoryResult;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    // 장소 이름으로 검색
+    @GET("v2/local/search/keyword.json")
+    Call<CategoryResult> getSearchLocation(
+            @Header("Authorization") String token,
+            @Query("query") String query,
+            @Query("size") int size
+    );
+
+    //카테고리로 검색
+    @GET("v2/local/search/category.json")
+    Call<CategoryResult> getSearchCategory(
+            @Header("Authorization") String token,
+            @Query("category_group_code") String category_group_code,
+            @Query("x") String x,
+            @Query("y") String y,
+            @Query("radius") int radius
+    );
+
+    //장소이름으로 특정위치기준으로 검색
+    @GET("v2/local/search/keyword.json")
+    Call<CategoryResult> getSearchLocationDetail(
+            @Header("Authorization") String token,
+            @Query("query") String query,
+            @Query("x") String x,
+            @Query("y") String y,
+            @Query("size") int size
+    );
+
+}
