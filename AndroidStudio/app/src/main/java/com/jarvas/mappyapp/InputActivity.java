a7179c434411ae6ccf207d35c03d4124c0c7f2e0
YeonSooYeon <dustndus8@naver.com>
2021-09-08 18:59:15 +0900

    feat: 도착지 검색창 구현

--- AndroidStudio/app/src/main/java/com/jarvas/mappyapp/InputActivity.java
+++ AndroidStudio/app/src/main/java/com/jarvas/mappyapp/InputActivity.java
@@ -31,6 +31,7 @@ import retrofit2.Response;
 
 public class InputActivity extends AppCompatActivity {
     RecyclerView recyclerView1;
+    RecyclerView recyclerView2;
     EditText searchEdit1;
     EditText searchEdit2;
     EditText searchEdit3;
@@ -49,17 +50,23 @@ public class InputActivity extends AppCompatActivity {
         searchEdit2 = findViewById(R.id.editText3); //도착지
         searchEdit3 = findViewById(R.id.editText5); //경유지
         recyclerView1 = findViewById(R.id.recyclerview1);
+        recyclerView2 = findViewById(R.id.recyclerview2);
 
         ArrayList<Document> documentArrayList = new ArrayList<>(); //지역명 검색 결과 리스트
         LocationAdapter locationAdapter = new LocationAdapter(documentArrayList, getApplicationContext(), searchEdit1, recyclerView1);
+        LocationAdapter locationAdapter2 = new LocationAdapter(documentArrayList, getApplicationContext(), searchEdit2, recyclerView2);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false); //레이아웃매니저
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false); //레이아웃매니저
+
         recyclerView1.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL)); //아래구분선 세팅
         recyclerView1.setLayoutManager(layoutManager);
         recyclerView1.setAdapter(locationAdapter);
 
+        recyclerView2.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL)); //아래구분선 세팅
+        recyclerView2.setLayoutManager(layoutManager2);
+        recyclerView2.setAdapter(locationAdapter2);
 
-
-        // editText 검색 텍스처이벤트
+        // editText1(출발지) 검색 텍스처이벤트
         searchEdit1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
@@ -126,7 +133,82 @@ public class InputActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "검색리스트에서 장소를 선택해주세요", Toast.LENGTH_SHORT).show();
             }
         });
+
+        // editText2(도착지) 검색 텍스처이벤트
+        searchEdit2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+                // 입력하기 전에
+                recyclerView2.setVisibility(View.VISIBLE);
+                Log.e("test","test : editText2 동작");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                if (charSequence.length() >= 1) {
+                    // if (SystemClock.elapsedRealtime() - mLastClickTime < 500) {
+                    documentArrayList.clear();
+                    locationAdapter.clear();
+                    locationAdapter.notifyDataSetChanged();
+                    ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+                    Call<CategoryResult> call = apiInterface.getSearchLocation(getString(R.string.restapi_key), charSequence.toString(), 15);
+                    call.enqueue(new Callback<CategoryResult>() {
+                        @Override
+                        public void onResponse(@NotNull Call<CategoryResult> call, @NotNull Response<CategoryResult> response) {
+                            if (response.isSuccessful()) {
+                                assert response.body() != null;
+                                for (Document document : response.body().getDocuments()) {
+                                    locationAdapter.addItem(document);
+                                }
+                                locationAdapter.notifyDataSetChanged();
+                            }
+                            else{
+                                Log.e("test",response.message());
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(@NotNull Call<CategoryResult> call, @NotNull Throwable t) {
+
+                        }
+                    });
+                    //}
+                    //mLastClickTime = SystemClock.elapsedRealtime();
+                } else {
+                    if (charSequence.length() <= 0) {
+                        recyclerView2.setVisibility(View.GONE);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // 입력이 끝났을 때
+            }
+        });
+
+        searchEdit2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                } else {
+                    recyclerView2.setVisibility(View.GONE);
+                }
+            }
+        });
+        searchEdit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "검색리스트에서 장소를 선택해주세요", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
+
+
 
 
     }
