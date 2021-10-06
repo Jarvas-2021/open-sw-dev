27d8b2a4ea2f91d387115abb96190325b4fa57d4
YeonSooYeon <dustndus8@naver.com>
2021-10-06 14:09:02 +0900

    feat: TimePicker dialog 추가

--- AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/TimePopupActivity.java
+++ AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/TimePopupActivity.java
@@ -6,7 +6,9 @@ import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -14,13 +16,47 @@ import com.jarvas.mappyapp.R;
 
 public class TimePopupActivity extends Activity {
 
+    int h=0, mi=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_popup);
+
+        Button stButton = findViewById(R.id.stButton);
+        stButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //출발시간 TimePickerDialog 띄우기
+                showTime();
+            }
+        });
+
+        Button dtButton = findViewById(R.id.dtButton);
+        dtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //도착시간 TimePickerDialog 띄우기
+                showTime();
+            }
+        });
+
     }
 
 
+    void showTime() {
+        TimePickerDialog.OnTimeSetListener mTimeSetListener =
+                new TimePickerDialog.OnTimeSetListener() {
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Toast.makeText(getApplicationContext(),
+                                hourOfDay + ":" + minute, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                };
+        TimePickerDialog oDialog = new TimePickerDialog(this,
+                android.R.style.Theme_DeviceDefault_Light_Dialog,
+                mTimeSetListener, 0, 0, false);
+        oDialog.show();
+    }
 
     //확인 버튼 클릭
     public void mOnClose(View v){
@@ -31,6 +67,20 @@ public class TimePopupActivity extends Activity {
 
         //액티비티(팝업) 닫기
         finish();
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()== MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
     }
 
 }
