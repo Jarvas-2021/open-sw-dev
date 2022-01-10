55890372ddef324fb7478925a04af34d8b3c6546
dbswl067 <nohyg7482@gmail.com>
2022-01-11 04:17:18 +0900

    Update RouteController.java
    androidPage()에서 routeList()로 메소드 이름 변경
    public String androidPage(HttpServletRequest req, Model model)메소드를 List<Route>형식으로 반환 타입 변경

--- crawl/src/main/java/com/crawl/demo/controller/RouteController.java
+++ crawl/src/main/java/com/crawl/demo/controller/RouteController.java
@@ -5,8 +5,10 @@ import com.crawl.demo.domain.Route;
 import com.crawl.demo.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -21,15 +23,15 @@ public class RouteController {
 
     @GetMapping("android-api")
     @ResponseBody
-    public List<Route> androidPage() {
+    public List<Route> routeList() {
         InputRoad.setStart("인천대입구역");
         InputRoad.setEnd("인천대학교 정보기술대학");
         routeService.crawling();
         return routeService.findRoutes();
     }
-    /*
+
     @RequestMapping(value = "/android", method = RequestMethod.POST)
-    public String androidPage(HttpServletRequest req, Model model) {
+    public List<Route> androidPage(HttpServletRequest req) {
         System.out.println("서버에서 안드로이드 접속 요청함");
         try {
             String start = req.getParameter("출발지");
@@ -38,11 +40,10 @@ public class RouteController {
             InputRoad.setEnd(end);
             System.out.println("안드로이드에서 받아온 출발지 : " + start);
             System.out.println("안드로이드에서 받아온 도착지" + end);
-            model.addAllAttributes("android", start);
-            return "android";
         }catch (Exception e) {
             e.printStackTrace();
-            return "null";
         }
-    }*/
+        routeService.crawling();
+        return routeService.findRoutes();
+    }
 }
