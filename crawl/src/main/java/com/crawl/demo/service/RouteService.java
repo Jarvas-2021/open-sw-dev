afa4ddcf3f97851dd5f65ee2c429806b465bd6cc
dbswl067 <nohyg7482@gmail.com>
2022-01-13 22:12:26 +0900

    크롤링 시 창 안 뜨게 설정

--- crawl/src/main/java/com/crawl/demo/service/RouteService.java
+++ crawl/src/main/java/com/crawl/demo/service/RouteService.java
@@ -59,7 +59,7 @@ public class RouteService {
 
         //Driver SetUp
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--window-size=300,600");
+        options.addArguments("headless");
         options.addArguments("--disable-popup-blocking");
         driver = new ChromeDriver(options);
 
