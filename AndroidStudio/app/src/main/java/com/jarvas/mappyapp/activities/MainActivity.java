dce90a5d8fca798a0b33ab424f8698b4569dc18d
Moon-ki Kim <mkmkqoa@gmail.com>
2022-01-26 14:25:44 +0900

    Update MainActivity.java

--- AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/MainActivity.java
@@ -146,108 +146,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView = (TextView)findViewById(R.id.sttResult);
 
         intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
         mRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
-        mRecognizer.setRecognitionListener(listener);
-        mRecognizer.startListening(intent);
 
-
-        /*
-        textView = (TextView)findViewById(R.id.sttResult);
-        sttBtn = (Button) findViewById(R.id.sttStart);
-
-        intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
-
-        sttBtn.setOnClickListener(v -> {
-            mRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
-            mRecognizer.setRecognitionListener(listener);
-            mRecognizer.startListening(intent);
-        });*/
-        // ==== /for SR ====
+        /*SRR srr = new SRR();
+        Thread thread = new Thread(srr);
+        thread.start();*/
 
     }
 
-    // ==== for SR ====
-        private RecognitionListener listener = new RecognitionListener() {
-        @Override
-        public void onReadyForSpeech(Bundle params) {
-            Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",
-                    Toast.LENGTH_SHORT).show();
-        }
-        @Override
-        public void onBeginningOfSpeech() {}
 
-        @Override
-        public void onRmsChanged(float rmsdB) {}
-
-        @Override
-        public void onBufferReceived(byte[] buffer) {}
-
-        @Override
-        public void onEndOfSpeech() {}
-
-        @Override
-        public void onError(int error) {
-            String message;
-
-            switch (error) {
-                case SpeechRecognizer.ERROR_AUDIO:
-                    message = "오디오 에러";
-                    break;
-                case SpeechRecognizer.ERROR_CLIENT:
-                    message = "클라이언트 에러";
-                    break;
-                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-                    message = "퍼미션 없음";
-                    break;
-                case SpeechRecognizer.ERROR_NETWORK:
-                    message = "네트워크 에러";
-                    break;
-                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-                    message = "네트웍 타임아웃";
-                    break;
-                case SpeechRecognizer.ERROR_NO_MATCH:
-                    message = "찾을 수 없음";
-                    break;
-                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-                    message = "RECOGNIZER가 바쁨";
-                    break;
-                case SpeechRecognizer.ERROR_SERVER:
-                    message = "서버가 이상함";
-                    break;
-                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-                    message = "말하는 시간초과";
-                    break;
-                default:
-                    message = "알 수 없는 오류임";
-                    break;
-            }
-
-            Toast.makeText(getApplicationContext(),
-                    "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onResults(Bundle results) {
-            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
-            ArrayList<String> matches =
-                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-
-            for(int i = 0; i < matches.size() ; i++){
-                textView.setText(matches.get(i));
-            }
-        }
-
-        @Override
-        public void onPartialResults(Bundle partialResults) {}
-
-        @Override
-        public void onEvent(int eventType, Bundle params) {}
-    };
-    // ==== /for SR ====
 
 
 
@@ -836,4 +743,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    class SRR implements Runnable{
+        SRR(){}
+
+        public void run(){
+            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+            mRecognizer.setRecognitionListener(listener);
+            mRecognizer.startListening(intent);
+        }
+        // ==== for SR ====
+        private RecognitionListener listener = new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+                Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onBeginningOfSpeech() {}
+
+            @Override
+            public void onRmsChanged(float rmsdB) {}
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {}
+
+            @Override
+            public void onEndOfSpeech() {}
+
+            @Override
+            public void onError(int error) {
+                String message;
+
+                switch (error) {
+                    case SpeechRecognizer.ERROR_AUDIO:
+                        message = "오디오 에러";
+                        break;
+                    case SpeechRecognizer.ERROR_CLIENT:
+                        message = "클라이언트 에러";
+                        break;
+                    case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                        message = "퍼미션 없음";
+                        break;
+                    case SpeechRecognizer.ERROR_NETWORK:
+                        message = "네트워크 에러";
+                        break;
+                    case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                        message = "네트웍 타임아웃";
+                        break;
+                    case SpeechRecognizer.ERROR_NO_MATCH:
+                        message = "찾을 수 없음";
+                        break;
+                    case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                        message = "RECOGNIZER가 바쁨";
+                        break;
+                    case SpeechRecognizer.ERROR_SERVER:
+                        message = "서버가 이상함";
+                        break;
+                    case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                        message = "말하는 시간초과";
+                        break;
+                    default:
+                        message = "알 수 없는 오류임";
+                        break;
+                }
+
+            /*Toast.makeText(getApplicationContext(),
+                    "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();*/
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+                ArrayList<String> matches =
+                        results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                for(int i = 0; i < matches.size() ; i++){
+                    textView.setText(matches.get(i));
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {}
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {}
+        };
+        // ==== /for SR ====
+    }
+
 }
+
