50323945e0b845a6702bb451980f2322c8f6916c
YeonSooYeon <dustndus8@naver.com>
2022-01-27 21:25:56 +0900

    feat: menu 추가

--- AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/StarActivity.java
+++ AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/StarActivity.java
@@ -1,4 +1,17 @@
 package com.jarvas.mappyapp.activities;
 
-public class StarActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jarvas.mappyapp.R;
+
+public class StarActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star);
+    }
 }
