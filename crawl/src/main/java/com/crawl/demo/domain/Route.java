42fed781ad497e2bfd82918cd48c5047c454ae4f
dbswl067 <nohyg7482@gmail.com>
2022-02-09 14:28:28 +0900

    변수 추가

--- crawl/src/main/java/com/crawl/demo/domain/Route.java
+++ crawl/src/main/java/com/crawl/demo/domain/Route.java
@@ -1,19 +1,17 @@
 package com.crawl.demo.domain;
 
 public class Route {
+
     private String time;
-    private String walkTime;
     private String path;
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
     private String price;
+
+    private String walkTime;
+    private String transfer;
+    private String km;
+
+    private String transType;
+    private String interTime;
 
     public String getTime() {
         return time;
@@ -21,6 +19,22 @@ public class Route {
 
     public void setTime(String time) {
         this.time = time;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
     }
 
     public String getWalkTime() {
@@ -31,11 +45,35 @@ public class Route {
         this.walkTime = walkTime;
     }
 
-    public String getPath() {
-        return path;
+    public String getTransfer() {
+        return transfer;
     }
 
-    public void setPath(String path) {
-        this.path = path;
+    public void setTransfer(String transfer) {
+        this.transfer = transfer;
+    }
+
+    public String getKm() {
+        return km;
+    }
+
+    public void setKm(String km) {
+        this.km = km;
+    }
+
+    public String getTransType() {
+        return transType;
+    }
+
+    public void setTransType(String transType) {
+        this.transType = transType;
+    }
+
+    public String getInterTime() {
+        return interTime;
+    }
+
+    public void setInterTime(String interTime) {
+        this.interTime = interTime;
     }
 }
