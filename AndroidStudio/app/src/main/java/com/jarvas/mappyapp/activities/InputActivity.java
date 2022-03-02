020c92f09cd2cae4737b10a544bd6fa2443a63d5
YeonSooYeon <dustndus8@naver.com>
2022-03-03 00:31:25 +0900

    refactor: 함수 순서 변경

--- AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/InputActivity.java
+++ AndroidStudio/app/src/main/java/com/jarvas/mappyapp/activities/InputActivity.java
@@ -1,42 +1,30 @@
 package com.jarvas.mappyapp.activities;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.jarvas.mappyapp.R;
 import com.jarvas.mappyapp.adapter.LocationAdapter;
-import com.jarvas.mappyapp.api.ApiClient;
-import com.jarvas.mappyapp.api.ApiInterface;
 import com.jarvas.mappyapp.listener.EventListener;
-import com.jarvas.mappyapp.model.category_search.CategoryResult;
 import com.jarvas.mappyapp.model.category_search.Document;
 import com.jarvas.mappyapp.utils.BusProvider;
-import com.jarvas.mappyapp.utils.ContextStorage;
 import com.jarvas.mappyapp.utils.IntentKey;
-import com.jarvas.mappyapp.utils.StringResource;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -44,10 +32,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class InputActivity extends AppCompatActivity {
     RecyclerView recyclerView1;
@@ -81,7 +65,8 @@ public class InputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_input);
         bus2.register(this);
         initView();
-        callbackActivity();
+        // todo - 서버 확인 후 callbackActivity 함수 제거
+        //callbackActivity();
         getProcessIntentAndKey();
     }
 
@@ -93,6 +78,7 @@ public class InputActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == RESULT_OK) {
+                            System.out.println("callbackActivity 함수 실행");
                             Intent intent = result.getData();
                             int CallType = intent.getIntExtra("CallType", 0);
                             if (CallType == 0) {
@@ -107,56 +93,19 @@ public class InputActivity extends AppCompatActivity {
                 });
     }
 
-    public void getProcessIntentAndKey() {
-        //Intent 받아오기
-        Intent processIntent = getIntent();
-        Bundle b = processIntent.getExtras();
-
-        //Key 값 받기
-        if (b!=null) {
-            Iterator<String> iter = b.keySet().iterator();
-            String key = "";
-            while (iter.hasNext()) {
-                key = iter.next();
-            }
-
-            switch (key) {
-                case IntentKey.PLACE_SEARCH_SET_STARTING:
-                    processIntent(processIntent, IntentKey.PLACE_SEARCH_SET_STARTING, searchEdit1, startAddressText, recyclerView1);
-                    break;
-                case IntentKey.PLACE_SEARCH_SET_DESTINATION:
-                    processIntent(processIntent, IntentKey.PLACE_SEARCH_SET_DESTINATION, searchEdit2, destinationAddressText, recyclerView2);
-                    break;
-            }
-
-
-        }
-    }
-
-    public void mOnPopupClick(View v) {
-        //데이터 담아서 팝업(액티비티) 호출
-        Intent intent = new Intent(getApplicationContext(), TimePopupActivity.class);
-        intent.putExtra("CallType", 1);
-        resultLauncher.launch(intent);
-    }
-
     private void initView() {
         //바인딩
         searchEdit1 = findViewById(R.id.editText);  //출발지
         searchEdit2 = findViewById(R.id.editText3); //도착지
-        //searchEdit3 = findViewById(R.id.editText5); //경유지
         recyclerView1 = findViewById(R.id.recyclerview1);
         recyclerView2 = findViewById(R.id.recyclerview2);
-        //recyclerView3 = findViewById(R.id.recyclerview3);
         okButton = findViewById(R.id.okButton);
 
         ArrayList<Document> documentArrayList = new ArrayList<>(); //지역명 검색 결과 리스트
         LocationAdapter locationAdapter = new LocationAdapter(documentArrayList, getApplicationContext(), searchEdit1, recyclerView1);
         LocationAdapter locationAdapter2 = new LocationAdapter(documentArrayList, getApplicationContext(), searchEdit2, recyclerView2);
-        //LocationAdapter locationAdapter3 = new LocationAdapter(documentArrayList, getApplicationContext(), searchEdit3, recyclerView3);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false); //레이아웃매니저
-        LinearLayoutManager layoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false); //레이아웃매니저
-        //LinearLayoutManager layoutManager3 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false); //레이아웃매니저
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
         recyclerView1.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL)); //아래구분선 세팅
         recyclerView1.setLayoutManager(layoutManager);
@@ -166,18 +115,15 @@ public class InputActivity extends AppCompatActivity {
         recyclerView2.setLayoutManager(layoutManager2);
         recyclerView2.setAdapter(locationAdapter2);
 
-//        recyclerView3.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL)); //아래구분선 세팅
-//        recyclerView3.setLayoutManager(layoutManager3);
-//        recyclerView3.setAdapter(locationAdapter3);
 
         // 검색 텍스처 Listener
         eventListener.addTextChangedListenerEvent(searchEdit1, recyclerView1, documentArrayList, locationAdapter);
-        eventListener.addTextChangedListenerEvent(searchEdit2,recyclerView2,documentArrayList,locationAdapter2);
+        eventListener.addTextChangedListenerEvent(searchEdit2, recyclerView2, documentArrayList, locationAdapter2);
         //eventListener.addTextChangedListenerEvent(searchEdit3,recyclerView3,documentArrayList,locationAdapter3);
 
         // setOnFocusChangeListener
-        eventListener.setOnFocusChangeListenerEvent(searchEdit1,recyclerView1);
-        eventListener.setOnFocusChangeListenerEvent(searchEdit2,recyclerView2);
+        eventListener.setOnFocusChangeListenerEvent(searchEdit1, recyclerView1);
+        eventListener.setOnFocusChangeListenerEvent(searchEdit2, recyclerView2);
         //eventListener.setOnFocusChangeListenerEvent(searchEdit3,recyclerView3);
 
         // setOnClickListener
@@ -185,54 +131,81 @@ public class InputActivity extends AppCompatActivity {
         eventListener.setOnClickListenerEvent(searchEdit2);
         //eventListener.setOnClickListenerEvent(searchEdit3);
 
-        okButton.setOnClickListener(new View.OnClickListener(){
+        okButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                Log.i("BUTTON","okButton click");
+            public void onClick(View view) {
+                Log.i("BUTTON", "okButton click");
                 getAddressText();
-                System.out.println("StartAddress: "+startAddressText+"DestAddress: "+destinationAddressText+"WayAddress: "+ wayPointAddressText);
+                System.out.println("StartAddress: " + startAddressText + "DestAddress: " + destinationAddressText + "WayAddress: " + wayPointAddressText);
                 putIntentAndStartActivity();
             }
         });
     }
 
     public void getAddressText() {
-        for(String str:set){
+        for (String str : set) {
             //출발지
-            if (Objects.equals(map.get(str),searchEdit1.getText().toString())){
+            if (Objects.equals(map.get(str), searchEdit1.getText().toString())) {
                 startAddressText = str;
             }
             //도착지
-            if (Objects.equals(map.get(str),searchEdit2.getText().toString())){
+            if (Objects.equals(map.get(str), searchEdit2.getText().toString())) {
                 destinationAddressText = str;
             }
-            //경유지
-//            if (Objects.equals(map.get(str),searchEdit3.getText().toString())){
-//                wayPointAddressText = str;
-//            }
+        }
+    }
+
+    public void getProcessIntentAndKey() {
+        //Intent 받아오기
+        Intent processIntent = getIntent();
+        Bundle b = processIntent.getExtras();
+
+        //Key 값 받기
+        if (b != null) {
+            Iterator<String> iter = b.keySet().iterator();
+            String key = "";
+            while (iter.hasNext()) {
+                key = iter.next();
+            }
+            switch (key) {
+                case IntentKey.PLACE_SEARCH_SET_STARTING:
+                    processIntent(processIntent, IntentKey.PLACE_SEARCH_SET_STARTING, searchEdit1, startAddressText, recyclerView1);
+                    break;
+                case IntentKey.PLACE_SEARCH_SET_DESTINATION:
+                    processIntent(processIntent, IntentKey.PLACE_SEARCH_SET_DESTINATION, searchEdit2, destinationAddressText, recyclerView2);
+                    break;
+            }
         }
     }
 
     public void putIntentAndStartActivity() {
         Intent intent = new Intent(InputActivity.this, ResultActivity.class);
-        intent.putExtra("startAddressText",startAddressText);
-        intent.putExtra("destinationAddressText",destinationAddressText);
+        intent.putExtra("startAddressText", startAddressText);
+        intent.putExtra("destinationAddressText", destinationAddressText);
         startActivity(intent);
     }
 
-
-    //검색예시 클릭시 이벤트 오토버스
-    @Subscribe
-    public void search(Document document) {
-        //public항상 붙여줘야함
-        Log.i("OTTO","ottobus event input activity");
-        Toast.makeText(getApplicationContext(), document.getPlaceName() + " 검색", Toast.LENGTH_SHORT).show();
-        System.out.println("search 이벤트 오토버스 실행 input");
-        mAddressText = document.getAddressName();
-        mPlaceNameText = document.getPlaceName();
-        map.put(mAddressText, mPlaceNameText);
+    private void processIntent(Intent intent, String key, EditText searchEdit, String addressText, RecyclerView recyclerView) {
+        if (intent != null) {
+            Document document = intent.getParcelableExtra(key);
+            if (document != null) {
+                getDocumentValues(document, searchEdit, addressText, recyclerView);
+            }
+        }
     }
 
+    private void getDocumentValues(Document document, EditText searchEdit, String addressText, RecyclerView recyclerView) {
+        searchEdit.setText(document.getPlaceName());
+        addressText = document.getAddressName();
+        recyclerView.setVisibility(View.GONE);
+    }
+
+    public void mOnPopupClick(View v) {
+        //데이터 담아서 팝업(액티비티) 호출
+        Intent intent = new Intent(getApplicationContext(), TimePopupActivity.class);
+        intent.putExtra("CallType", 1);
+        resultLauncher.launch(intent);
+    }
 
     @Override
     public void finish() {
@@ -245,45 +218,15 @@ public class InputActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private void processIntentStarting(Intent intent) {
-        if (intent != null) {
-            Document document = intent.getParcelableExtra(IntentKey.PLACE_SEARCH_SET_STARTING);
-            if (document != null) {
-                getDocumentValues(document,searchEdit1, startAddressText, recyclerView1);
-            }
-        }
+    //검색예시 클릭시 이벤트 오토버스
+    @Subscribe
+    public void search(Document document) {
+        //public항상 붙여줘야함
+        Log.i("OTTO", "ottobus event input activity");
+        Toast.makeText(getApplicationContext(), document.getPlaceName() + " 검색", Toast.LENGTH_SHORT).show();
+        System.out.println("search 이벤트 오토버스 실행 input");
+        mAddressText = document.getAddressName();
+        mPlaceNameText = document.getPlaceName();
+        map.put(mAddressText, mPlaceNameText);
     }
-
-    private void processIntentDestination(Intent intent) {
-        if (intent != null) {
-            Document document = intent.getParcelableExtra(IntentKey.PLACE_SEARCH_SET_DESTINATION);
-            if (document != null) {
-                getDocumentValues(document,searchEdit2, destinationAddressText, recyclerView2);
-            }
-        }
-    }
-//    private void processIntentWayPoint(Intent intent) {
-//        if (intent != null) {
-//            Document document = intent.getParcelableExtra(IntentKey.PLACE_SEARCH_SET_WAYPOINT);
-//            if (document != null) {
-//                getDocumentValues(document,searchEdit3, wayPointAddressText, recyclerView3);
-//            }
-//        }
-//    }
-
-    private void processIntent(Intent intent,String key, EditText searchEdit, String addressText, RecyclerView recyclerView) {
-        if (intent != null) {
-            Document document = intent.getParcelableExtra(key);
-            if (document != null) {
-                getDocumentValues(document,searchEdit, addressText, recyclerView);
-            }
-        }
-    }
-
-    private void getDocumentValues(Document document, EditText searchEdit, String addressText, RecyclerView recyclerView) {
-        searchEdit.setText(document.getPlaceName());
-        addressText = document.getAddressName();
-        recyclerView.setVisibility(View.GONE);
-    }
-
 }
